c86cb4a480478dee2b951361a14f5b844d34966a
정다연 <j84512@naver.com>
2025-10-23 09:46:47 +0900

    init

--- src/main/java/com/example/eureka_serv/EurekaServApplication.java
+++ src/main/java/com/example/eureka_serv/EurekaServApplication.java
@@ -2,12 +2,14 @@ package com.example.eureka_serv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServApplication.class, args);
-	}
+        public static void main(String[] args) {
+                SpringApplication.run(EurekaServApplication.class, args);
+        }
 
 }
